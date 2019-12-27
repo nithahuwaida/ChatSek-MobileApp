799278805c16c98a40d8192e64f64d259656be13
nithahuwaida <hafizhahuwaida@gmail.com>
2019-12-28 02:35:31 +0700

    Update splash screen

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, Image} from 'react-native';
 import * as firebase from 'firebase';
+import logoChat from '../../images/chatseklogo.png';
 
 export default class LoadingScreen extends React.Component{
     componentDidMount(){
@@ -12,8 +13,7 @@ export default class LoadingScreen extends React.Component{
     render(){
         return(
             <View style={styles.container}>
-                <Text>Loading...</Text>
-                <ActivityIndicator size="large"></ActivityIndicator>
+                <Image source={logoChat} style={styles.logoChatSek}/>
             </View>
         )
     }
@@ -24,5 +24,10 @@ const styles= StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: '#1B4F72'
+    },
+    logoChatSek :{
+        width:200,
+        height: 170
     }
 })
